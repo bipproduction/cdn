8b3d61ae9c445b8d89604b940ba0630c1df15643
bipproduction <bip.production.js@gmail.com>
2023-07-30 14:38:29 +0800

    tambahan

--- x.sh
+++ x.sh
@@ -1,1 +1,18 @@
-echo "apa kabarnya"
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    
+    root /var/www/html;
+    
+    # Add index.php to the list if you are using PHP
+    index index.html index.htm index.nginx-debian.html;
+    
+    server_name _;
+    
+    location / {
+        
+        try_files $uri $uri/ =404;
+    }
+    
+    
+}
